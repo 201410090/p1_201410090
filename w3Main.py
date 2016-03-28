04bcfee85d4c7a4cde6e6fb87081a0c122046265
201410090 <ehwkrl624@naver.com>
2016-03-28 15:59:00 +0900

    Added files via upload

---
+++ w3Main.py
@@ -0,0 +1,33 @@
+﻿import turtle
+
+wn=turtle.Screen()
+
+t1=turtle.Turtle()
+
+ 
+
+def draw(size, angle, sides):
+
+    for s in range(sides):
+
+        t1.forward(size)
+
+        t1.left(angle)
+
+ 
+
+sel=raw_input("Triangle or Square:")
+
+if(sel=='T'):
+
+    print "You entered T"
+
+    draw(100, 120, 3)
+
+else:
+
+    print "You entered S"
+
+    draw(100, 90, 4)
+
+wn.exitonclick() 
