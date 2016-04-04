d6c74db07eb21d6d6f58019c64676fcc413f6b8f
lee giri <ehwkrl624@naver.com>
2016-04-04 13:52:37 +0900

    BMI

---
+++ w5main2.py
@@ -0,0 +1,30 @@
+﻿"""
+
+@author lgr
+@since160404
+"""
+
+def computeBMI(height,weight):
+	
+	bmi=weight/(height*height)
+	print bmi
+	if bmi>=18.5 and bmi<25.0:
+    		print "namal weight"
+	elif bmi>=25.0 and bmi<30.0:
+    		print "over weight"
+	else: 
+    		print "error"
+
+def lab5():
+	computeBMI
+	height=1.8
+	weight=75
+	computeBMI(height,weight)
+
+def main():
+	lab5()
+
+if__name__=="__main__"
+	main()
+
+raw_input()
