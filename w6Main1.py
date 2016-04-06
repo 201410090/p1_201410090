e3233ece30f6d9e166b27fae2692163df293e1fe
lee giri <ehwkrl624@naver.com>
2016-04-06 15:17:48 +0900

    sumMultiful

---
+++ w6Main1.py
@@ -0,0 +1,12 @@
+﻿def sumMultiful3_5(begin,end):
+    sum=0
+    for i in range(begin,end):
+        if i%3==0 or i%5==0:
+            sum=sum + i
+
+    print sum
+    return sum
+
+sumMultiful3_5(0,1000)
+
+raw_input()
