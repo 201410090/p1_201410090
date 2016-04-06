0c85fe8684c1c5c9122d26f17d5ae78f41980f6a
lee giri <ehwkrl624@naver.com>
2016-04-06 15:44:26 +0900

    leapYear

---
+++ w6Main10.py
@@ -0,0 +1,8 @@
+﻿def findLeapyear():
+    sel = int(raw_input("pressed year: "))
+    if (sel%4==0) and (sel%100!=0 or sel%400==0):
+        print "This is leapyear"
+    else:
+        print "This is not leapyear"
+        
+findLeapyear()
