bf20ac6bcaa30af6c89545ae5b2f6d97cda7bbb9
lee giri <ehwkrl624@naver.com>
2016-04-08 18:48:04 +0900

    up and down game

---
+++ upanddwon.py
@@ -0,0 +1,19 @@
+﻿def upanddown():
+    target = input("pressed target: ")
+    count = 0
+    while(1):
+        number = input("pressed number: ")
+        count = count+1
+        if(target == number):
+            print "number"
+            print count
+            break
+        else:
+            if(target>number):
+                print "up"
+            else:
+                print "down"
+
+upanddown()
+
+raw_input()
