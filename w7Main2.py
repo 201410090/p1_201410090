09ce3c5840336971ea1c7b6b1bf05b9bc005b52a
lee giri <ehwkrl624@naver.com>
2016-04-18 23:59:39 +0900

    Added files via upload

--- w7Main2.py
+++ w7Main2.py
@@ -1,35 +1,33 @@
-﻿import turtle
-wn=turtle.Screen()
-t1=turtle.Turtle()
+﻿import turtle 
+wn=turtle.Screen() 
+t1=turtle.Turtle() 
+ 
+def drawSquareAtSave(size,pos): 
+    tracks=list() 
+    t1.penup() 
+    t1.setpos(pos) 
+    for i in range(0,4): 
+        t1.fd(size) 
+        t1.left(90) 
+        tracks.append(t1.pos()) 
+    return tracks 
 
-def drawSquareAtSave(size,pos):
-    tracks=list()
-    t1.penup()
-    t1.setpos(pos)
-    for i in range(0,4):
-	    t1.fd(size)
-	    t1.left(90)
-            tracks.append(t1.pos())
-    return tracks
+def lab7(): 
+    size=100 
+    pos=t1.pos() 
+    mytracks=drawSquareAtSave(size,pos) 
+    drawSquareFront_list(mytracks) 
+    print mytracks 
+
+def main(): 
+    lab7() 
+    
+def drawSquareFront_list(tracks): 
+    t1.penup() 
+    t1.goto(tracks[3]) 
+    t1.pendown() 
+    for i in range(4): 
+        t1.goto(tracks[i])
         
-def lab7():
-    size=100
-    pos=t1.pos()
-    mytracks=drawsquareAtSave(size,pos)
-    drawSquareFront_list(mytracks)
-    print mytracks
-
-def main():
-    lab7()
-
-def drawSquareFront_list():
-	t1.penup()
-	t1.tracks(tracks[3])
-	t1.pendown()
-	for i in range(4):
-		t1.goto(tracks[i])
-
 if __name__=="__main__":
-    main()
-
-raw_input()
+    main() 
