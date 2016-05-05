1124439942c4ea0af4a51672395ccc67c7c94cfc
lee giri <ehwkrl624@naver.com>
2016-05-06 00:04:17 +0900

    Added files via upload

---
+++ w8Main4.py
@@ -0,0 +1,17 @@
+﻿import math
+
+myLocation=list()
+Locations=list()
+distances=list()
+
+myLocation=[(37.575869, 126.976637)]
+Locations=[(37.576549, 126.985520),(37.570426, 126.992118),(37.570175, 126.982998),(37.571618, 126.976551)]
+
+for i in range(0,len(Locations)):
+    distances.append(math.sqrt((myLocation[0][0]-Locations[i][0])**2+(myLocation[0][1]-Locations[i][1])**2))
+
+print distances
+
+print min(distances)
+
+raw_input()
