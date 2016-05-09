d2b6eb20a93de2a393b001aed9477a455d821bca
lee giri <ehwkrl624@naver.com>
2016-05-09 13:05:05 +0900

    Added files via upload

---
+++ w10Main.py
@@ -0,0 +1,27 @@
+﻿allData=[
+    ["Coffee","Water","Milk","Icecream"], 
+    ["Espresso","No","No","No"],
+    ["Long Black","Yes","No","No"],
+    [ "Flat White","No","Yes","No"],
+    ["Cappuccino","No","Yes","No"],
+    ["Affogato","No","No","Yes"]
+]
+
+data = allData[1:]
+print data
+print "coffe count =", len(data)
+
+
+for i in range(0,len(data)):
+    print data[i][0], data[i][2]
+
+
+for i in data:
+    if i[2].upper()=="YES":
+        print "milk coffe =", i[0]
+
+print "milk coffe count =", len(i[2])
+
+print (float(len(i[2]))/float(len(data)))
+
+raw_input()
