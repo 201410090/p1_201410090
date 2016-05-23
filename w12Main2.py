2b38b3adf6f891e38cd357e11c53194d60556ff0
lee giri <ehwkrl624@naver.com>
2016-05-23 20:44:02 +0900

    Add files via upload

---
+++ w12Main2.py
@@ -0,0 +1,8 @@
+﻿myfile=open('output.txt','w')
+line1='first line \n'
+myfile.write(line1.upper())
+line2='second line \n'
+myfile.write(line2.upper())
+line3='third'
+myfile.write(line3.upper())
+myfile.close()
