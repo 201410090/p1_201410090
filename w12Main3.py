6db6788d2058af0dd912da90d860879ec2ad97c2
lee giri <ehwkrl624@naver.com>
2016-05-26 01:38:15 +0900

    Add files via upload

---
+++ w12Main3.py
@@ -0,0 +1,17 @@
+﻿import time
+fin=open('output.txt','r')
+fout=open('outputUpper.txt','w')
+
+for line in fin:
+    sentense = ""
+    words=line.split()
+    for word in words:
+        if word=='line':
+            word=word.upper()
+            sentense =  "[{0} edited {1}]".format(editor, timeEdited) + " " + sentense
+        sentense = sentense + " " + word
+    fout.write(sentense)
+fin.close()
+fout.close()
+
+%outputUpper.txt
