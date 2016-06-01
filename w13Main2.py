9dc5df052ace983840d1d8c79b0a8085d0010518
lee giri <ehwkrl624@naver.com>
2016-06-01 19:16:06 +0900

    Add files via upload

---
+++ w13Main2.py
@@ -0,0 +1,38 @@
+﻿class Dog(object): 
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is", self.name
+    def talk(self):
+        print self.name, "is mung mung"
+        
+class ShihTzudog(object): 
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is", self.name
+    def talk(self):
+        print self.name, "is si si"
+        
+class Maltese(object): 
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is", self.name
+    def talk(self):
+        print self.name, "is mal mal"
+
+mydogname='poppy'
+mydog=Dog(mydogname)
+mydog.getName()
+mydog.talk()
+
+mydogname='poppy'
+mydog=ShihTzudog(mydogname)
+mydog.getName()
+mydog.talk()
+
+mydogname='poppy'
+mydog=Maltese(mydogname)
+mydog.getName()
+mydog.talk()
